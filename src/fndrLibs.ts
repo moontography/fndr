850d45f3e148066a56fbee9384bcf75ffa56cfdd
Lance Whatley <whatl3y@gmail.com>
2021-02-28 15:45:26 -0500

    Include connectors and commands in exports to be used elsewhere

--- src/fndrLibs.ts
+++ src/fndrLibs.ts
@@ -1,4 +1,7 @@
+import Commands from './commands'
+import Connectors from './connectors'
+import Config from './libs/Config'
 import Encryption from './libs/Encryption'
 import FileManagement from './libs/FileManagement'
 
-export { Encryption, FileManagement }
+export { Commands, Config, Connectors, Encryption, FileManagement }
